f4e5ba3813071aa5bf6b96a8a488d577204e75b2
papua <nakao_shinichirou@aratana.jp>
2015-01-12 18:33:07 +0900

    add : save User into db

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -24,6 +24,7 @@ object UserController extends Controller {
 
   def register = Action { implicit request =>
     val user = form.bindFromRequest.get
+    user.save
     Ok(views.html.user.confirm("User registered name=\"" + user.name + "\" mail=\"" + user.mail + "\"."))
   }
 }
--- app/models/User.scala
+++ app/models/User.scala
@@ -1,6 +1,26 @@
 package models
 
+import play.api.db.DB
+import anorm._
+import play.api.Play.current
+
 /**
  * Created by papua on 2015/01/09.
  */
-case class User (name:String, mail:String)
+case class User (name:String, mail:String) {
+  def save = {
+    DB.withConnection { implicit c =>
+      SQL(
+        """
+            INSERT INTO user_table(name, mail)
+            VALUES({name}, {mail})
+        """
+      ).on('name -> name,
+          'mail -> mail).execute()
+    }
+
+//    DB.withTransaction { implicit conn =>
+//      // このブロックの処理は1つのトランザクションとなります
+//    }
+  }
+}
