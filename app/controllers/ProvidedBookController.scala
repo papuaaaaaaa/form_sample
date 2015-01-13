83b32c8b722bb83eb175e1091d374bdfa7f3f326
papua <nakao_shinichirou@aratana.jp>
2015-01-13 23:43:35 +0900

    modify : display search word form for provided books

--- app/controllers/ProvidedBookController.scala
+++ app/controllers/ProvidedBookController.scala
@@ -4,10 +4,15 @@
 
 package controllers
 
+import models.User
 import play.api.mvc._
 
 object ProvidedBookController extends Controller{
   def index = Action {
-    Ok(views.html.provided.index("provided books"))
+    Ok(views.html.provided.index("provided books", User.providedBooks))
+  }
+
+  def add = Action {
+    Ok(views.html.common.form("search provided books!!!"))
   }
 }
