5444d7f1aea05056bfd6aa29b9444761f08e040b
papua <nakao_shinichirou@aratana.jp>
2015-01-14 17:10:35 +0900

    modify : insert into database from books list

--- app/controllers/ProvidedBookController.scala
+++ app/controllers/ProvidedBookController.scala
@@ -4,15 +4,47 @@
 
 package controllers
 
-import models.User
-import play.api.mvc._
+import models.{AmazonProductAdvertisting, Book, User}
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.mvc.{Action, Controller}
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object ProvidedBookController extends Controller{
+  val form = Form (
+    mapping(
+      "title" -> text,
+      "author" -> text,
+      "isbn" -> text,
+      "imageUrl" -> text
+    )(Book.apply)(Book.unapply)
+  )
+
   def index = Action {
     Ok(views.html.provided.index("provided books", User.providedBooks))
   }
 
   def add = Action {
     Ok(views.html.common.search_form("search provided books!!!"))
+  }
+
+
+  def search = Action.async { implicit request =>
+    val form = Form("keyword" -> text)
+    val keyword = form.bindFromRequest.get
+    AmazonProductAdvertisting.search(keyword).map(res => {
+      val items = AmazonProductAdvertisting.parseXml(res.xml)
+      Ok(views.html.wanted.index(keyword, items, res.xml.toString))
+    })
+  }
+
+  def register = Action { implicit request =>
+    val book = form.bindFromRequest.get
+    if (book.save > 0) {
+      Redirect("/provided_books")
+    } else {
+      Redirect("/")
+    }
   }
 }
--- app/models/Book.scala
+++ app/models/Book.scala
@@ -1,6 +1,30 @@
 package models
 
+import anorm._
+import play.api.db.DB
+import play.api.Play.current
+
 /**
  * Created by papua on 2015/01/13.
  */
-class Book(val title:String, val author:String, val isbn:String, val imageUrl:String)
+case class Book(val title:String, val author:String, val isbn:String, val imageUrl:String) {
+  def save = {
+    DB.withConnection { implicit c =>
+      SQL(
+        """
+          INSERT INTO provided_book_table(user_id, title, author, isbn, image_url) VALUES({userId}, {title}, {author}, {isbn}, {imageUrl});
+        """
+      ).on(
+          'userId -> -1,
+          'title -> title,
+          'author -> author,
+          'isbn -> isbn,
+          'imageUrl -> imageUrl
+        ).executeUpdate()
+    }
+
+    def delete = {
+
+    }
+  }
+}
